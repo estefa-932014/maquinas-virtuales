9fbd8d919122e993ba594b0f19d05b8b8887d050
esnieto <esnieto@bancolombia.com.co>
2024-03-05 18:15:11 -0500

    Mejora continua

--- vagrant_mejora_continua/Vagrant/deploy-app.sh
+++ vagrant_mejora_continua/Vagrant/deploy-app.sh
@@ -1,7 +1,5 @@
 cd /practica/ejercicio2/ 
 docker-compose up -d --build
-pwd
-ls
 echo "configurando ngrok"
 ngrok config add-authtoken 2WuFTtObR3Q4WhNEKEbzgZWoLAa_5D1v8CvBm44CNVmQFivzn
 ngrok http http://localhost:8080
